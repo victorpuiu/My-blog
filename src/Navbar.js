be4203620e1e014ad86fb38e2424de64bc640d51
victorpuiu <puiu.danut@gmail.com>
2023-03-04 17:02:55 +0200

    adding css in index.css for components

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,8 +5,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The React Blog</h1>
             <div className="links">
-                <a href={ "/" }>Home</a>
-                <a href={ "/create" }>New Blog</a>
+                <a href="/">Home</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor: "#f1356d",
+                    borderRadius: "8px"
+                }}>New Blog</a>
 
             </div>
         </nav>
