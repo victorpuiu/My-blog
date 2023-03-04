4bebf6244acb8e1173eee7f099f8f5c0e1def79e
victorpuiu <puiu.danut@gmail.com>
2023-03-04 19:09:17 +0200

    adding click events on Homepage component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,15 @@
+import {useState} from "react"; //useState is a Hook, make a value reactive and use it whenever we want
+
 
 const Home  = () => {
+    // let name = "stef";
+    const [name, setName] = useState("stef");
+    const [age, setAge] = useState(25);
+
 
     const handleClick = () => {
-        console.log("hello, codecool");
+        setName("luigi");
+        setAge(35);
     }
 
 
@@ -10,6 +17,7 @@ const Home  = () => {
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={ handleClick }>Click me</button>
         </div>
     );
