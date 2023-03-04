129bfb96a97706b1a8827a3269cc92ba0ee82cef
victorpuiu <puiu.danut@gmail.com>
2023-03-04 19:53:06 +0200

    outputting list of blogs
    updating Homepage and index.css

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,21 @@
-const Home  = () => {
+import {useState} from "react";
 
+const Home  = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "My new website", body: "lorem ipsum", author: "stef", id: 1},
+        {title: "Welcome party", body: "lorem ipsum", author: "dan", id: 2},
+        {title: "Web dev top tips ", body: "lorem ipsum", author: "victor", id: 3}
+
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className={"blog-preview"} key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by  {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
