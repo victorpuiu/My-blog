1a89014d6228e5b597e03a6ba2c684d34e4d429b
victorpuiu <puiu.danut@gmail.com>
2023-03-04 15:44:08 +0200

    update app template with dynamic values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
 function App() {
+
+    const title = "Welcome to the new blog";
+    const likes = 50;
+    // const person = {name: "Dan", age: 30}
+    const link = "http://google.com"
+
   return (
     <div className="App">
       <div className="content">
-          <h1>App component</h1>
+          <h1>{ title }</h1>
+          <p>Liked { likes } times</p>
+          {/*<p>{ person }</p>*/}
+
+          <a href={ link }>Google sites</a>
       </div>
     </div>
   );
