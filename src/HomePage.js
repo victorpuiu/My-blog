23f74534ab282fb3f23456efc622494967c76015
victorpuiu <puiu.danut@gmail.com>
2023-03-05 14:06:19 +0200

    setting up a json server and create a db.json file for simulate a database

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,8 +6,9 @@ const Home  = () => {
         {title: "My new website", body: "lorem ipsum", author: "stef", id: 1},
         {title: "Welcome party", body: "lorem ipsum", author: "dan", id: 2},
         {title: "Web dev top tips ", body: "lorem ipsum", author: "stef", id: 3}
-
     ]);
+
+    // const [name, setName] = useState("stef");
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -16,14 +17,16 @@ const Home  = () => {
 
     useEffect(() => {
         console.log("use effect run");
-        console.log(blogs);
-    });
+        // console.log(name);
+
+    }, []);
 
     return (
         <div className="home">
             <BlogList blogs={ blogs } title = "All Blogs!" handleDelete={ handleDelete }/>
             {/*<BlogList blogs={blogs.filter((blog) => blog.author === "stef")} title = "Stef's Blogs!"/>*/}
-
+            {/*<button onClick={() => setName("luigi")}>change name</button>*/}
+            {/*<p>{name}</p>*/}
         </div>
     );
 }
