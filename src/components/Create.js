ddd52c57f516bddd5800cba2773d7c117eabe2e4
victorpuiu <puiu.danut@gmail.com>
2023-03-06 00:09:02 +0200

    adding Create.js component(form component)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,8 +1,52 @@
+import {useState} from "react";
+
 const Create = () => {
+
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={ handleSubmit }>
+
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => {setTitle(e.target.value)}}
+                />
+
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={ body }
+                    onChange={ (e) => {setBody(e.target.value)}}
+                    >
+                </textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => {setAuthor(e.target.value)}}
+
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+
+                <button>Add blog</button>
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
         </div>
 
     );
