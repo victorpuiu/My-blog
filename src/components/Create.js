55068a6e14cb212f588863c42ed3c7d530f76054
victorpuiu <puiu.danut@gmail.com>
2023-03-06 00:44:54 +0200

    redirect to the home page after adding a blog

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import {useHistory} from "react-router-dom";
 
 const Create = () => {
 
@@ -6,6 +7,7 @@ const Create = () => {
     const [body, setBody] = useState("");
     const [author, setAuthor] = useState("mario");
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,6 +22,8 @@ const Create = () => {
         }).then(() => {
             console.log("new blog added");
             setIsPending(false);
+            // history.go(-1);
+            history.push("/");
         })
 
     }
