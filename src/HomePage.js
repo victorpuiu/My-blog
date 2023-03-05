1a80e7ed99f42b8f4d0e4863c73f87bf6887364b
victorpuiu <puiu.danut@gmail.com>
2023-03-05 15:22:56 +0200

    conditional outputting and loading template

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,22 +3,27 @@ import BlogList from "./BlogList";
 
 const Home  = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
 
     useEffect(() => {
-        fetch(" http://localhost:8000/blogs")
-            .then(res => {   //res is response object
-                return res.json();  //return and get the data
-            })
-            .then((data) => {
-                console.log(data);
-                setBlogs(data);
-            })
+        setTimeout(() => {
+            fetch(" http://localhost:8000/blogs")
+                .then(res => {   //res is response object
+                    return res.json();  //return and get the data
+                })
+                .then((data) => {
+                    console.log(data);
+                    setBlogs(data);
+                    setIsPending(false);
+                })
+        }, 1000);
 
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={ blogs } title = "All Blogs!" />}
 
         </div>
