bb171fc683d6b8512f4edff3682b499acba081a3
victorpuiu <puiu.danut@gmail.com>
2023-03-05 20:24:23 +0200

    updating router link on Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
 
@@ -5,12 +6,12 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Stef Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#f1356d",
                     borderRadius: "8px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
 
             </div>
         </nav>
